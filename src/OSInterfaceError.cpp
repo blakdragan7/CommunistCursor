9bd8cd55e89e60505e7c6b8d1991ceaa08889473
Jonathon Copeland <jcopela4@gmail.com>
2020-04-18 16:03:04 -0400

    Added more OSInterfaceErrors

--- src/OSInterfaceError.cpp
+++ src/OSInterfaceError.cpp
@@ -1,6 +1,6 @@
 #include "OSInterfaceError.h"
 
-std::string MouseInterfaceErrorToString(OSInterfaceError err)
+std::string OSInterfaceErrorToString(OSInterfaceError err)
 {
     switch(err)
     {
@@ -8,8 +8,14 @@ std::string MouseInterfaceErrorToString(OSInterfaceError err)
             return "Error Success";
         case OS_E_ALREADY_REGISTERED:
             return "Error Already Registered";
+        case OS_E_INVALID_PARAM:
+            return "Error Invalid Paramater Passed To Function";
         case OS_E_NOT_REGISTERED:
             return "Error Not Registered";
+        case OS_E_NOT_AUTHERIZED:
+            return "Error Not Autherized";
+        case OS_E_UNKOWN:
+            return "Error Unkown Error";
         default:
             return "Invalid Error";
     }
--- src/OSInterfaceError.h
+++ src/OSInterfaceError.h
@@ -6,8 +6,11 @@
 enum OSInterfaceError
 {
     OS_E_SUCCESS,
+    OS_E_INVALID_PARAM,
     OS_E_ALREADY_REGISTERED,
     OS_E_NOT_REGISTERED,
+    OS_E_NOT_AUTHERIZED,
+    OS_E_UNKOWN
 };
 
 extern std::string OSInterfaceErrorToString(OSInterfaceError err);
