436961dc8299c816ffcbb58bcf0c61da4908889e
Jonathon Copeland <jcopela4@gmail.com>
2020-06-23 21:49:14 -0400

    added basic math and collision testing to BasicTypes

--- src/Service/CC/BasicTypes.h
+++ src/Service/CC/BasicTypes.h
@@ -6,6 +6,21 @@ struct Point
     int x,y;
     Point() :x(0), y(0) {}
     Point(int x, int y) : x(x), y(y) {}
+
+    Point operator-(const Point& rh)
+    {
+        return Point(x - rh.x, y - rh.y);
+    }
+
+    Point operator+(const Point& rh)
+    {
+        return Point(x + rh.x, y + rh.y);
+    }
+
+    Point operator/(const int& rh)
+    {
+        return Point(x / rh, y / rh);
+    }
 };
 
 struct Rect
@@ -16,6 +31,14 @@ struct Rect
     Rect() {}
     Rect(int tlx, int tly, int brx, int bry) : topLeft(tlx,tly), bottomRight(brx,bry) {}
     Rect(Point tl, Point br) : topLeft(tl), bottomRight(br) {}
+
+    bool IntersectsRect(const Rect& other)
+    {
+        return !(other.topLeft.x > bottomRight.x || \
+                 other.bottomRight.x < topLeft.x || \
+                 other.topLeft.y > bottomRight.y || \
+                 other.bottomRight.y < topLeft.y);
+    }
 };
 
 #endif
